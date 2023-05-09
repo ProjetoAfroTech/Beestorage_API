bf5316a1cffad108ec1d37e61a7ab46012585857
Luana Carneiro <luanamaria2742@gmail.com>
2023-05-09 13:02:48 -0300

    first commit

---
+++ src/main/java/com/beestorage/BeestorageApplication.java
@@ -0,0 +1,13 @@
+package com.beestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeestorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeestorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beestorage/BeestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
