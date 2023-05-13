2c69279b7e22df409334ed513c3272f5fa615bb9
Gracii Ferreira <graciele.af26@gmail.com>
2023-05-12 21:44:47 -0300

    usuario

---
+++ src/main/java/com/beestorage/model/UsuarioModel.java
@@ -0,0 +1,90 @@
+package com.beestorage.model;
+
+import java.util.List;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_usuario")
+public class UsuarioModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@NotNull(message = "O atributo nomeUsuario é obrigatório e não pode utilizar espaços em branco!")
+	public String nomeUsuario;
+	
+	//@Schema(example = "email@email.com")
+	@NotNull(message = "O atributo usuário é obrigatório e não pode utilizar espaços em branco!") 
+	@Email(message="O atributo e-mail é obrigatório")
+	public String usuario;
+	
+	@NotBlank(message = "O atributo senha é obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 8, message = "O atributo senha deve conter no mínimo 08 caracteres")
+	public String senha;
+
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE,fetch = FetchType.EAGER)
+	@JsonIgnoreProperties("usuario")
+	private List <ProdutoModel> produto;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+		
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+		
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+		
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	public List<ProdutoModel> getProduto() {
+		return produto;
+	}
+	public void setProduto(List<ProdutoModel> produto) {
+		this.produto = produto;
+	}
+	
+}
