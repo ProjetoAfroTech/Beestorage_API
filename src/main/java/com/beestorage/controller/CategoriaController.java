199b54e7f5658c62e88d2dd70d35f2c27003f2c3
Leticia Affonso <le.affonsosoares.gmail.com>
2023-05-12 21:27:27 -0300

    estrutura categoria realizada

--- src/main/java/com/beestorage/controller/CategoriaController.java
+++ src/main/java/com/beestorage/controller/CategoriaController.java
@@ -1,5 +1,74 @@
 package com.beestorage.controller;
 
-public class CategoriaController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.beestorage.model.CategoriaModel;
+import com.beestorage.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaModel>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriaModel> getById(@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/buscar/{categoria}")
+	public ResponseEntity<List<CategoriaModel>> getByName(@PathVariable String categoria){
+		return ResponseEntity.ok(repository.findAllByNomeCategoriaContainingIgnoreCase(categoria));
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoriaModel> postTema(@Valid @RequestBody CategoriaModel categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	} 
+	
+	@PutMapping
+	public ResponseEntity<CategoriaModel> put (@RequestBody CategoriaModel categoria){
+		if (repository.existsById(categoria.getId())) {
+			return ResponseEntity.ok(repository.save(categoria));
+		}
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<CategoriaModel> categoria = repository.findById(id);
+		if (categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		repository.deleteById(id);
+		
+	}
 }
--- src/main/java/com/beestorage/model/CategoriaModel.java
+++ src/main/java/com/beestorage/model/CategoriaModel.java
@@ -1,5 +1,77 @@
 package com.beestorage.model;
 
-public class CategoriaModel {
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria") 
+public class CategoriaModel {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	@NotBlank (message = "O atributo categorias é obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 5, max = 20, message = "O atributo categorias deve conter no mínimo 05 e no máximo 20 caracteres")
+	private String nomeCategoria;
+	
+	@NotBlank (message = "O atributo categorias é obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 5, max = 20, message = "O atributo categorias deve conter no mínimo 05 e no máximo 20 caracteres")
+	private String subCategoria;
+	
+	@NotNull
+	private int volumes;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE,fetch = FetchType.EAGER)
+	@JsonIgnoreProperties("categoria")
+	private List <ProdutoModel> produto;
+	
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getSubCategoria() {
+		return subCategoria;
+	}
+
+	public void setSubCategoria(String subCategoria) {
+		this.subCategoria = subCategoria;
+	}
+
+	public int getVolumes() {
+		return volumes;
+	}
+
+	public void setVolumes(int volumes) {
+		this.volumes = volumes;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+	
+	
 }
